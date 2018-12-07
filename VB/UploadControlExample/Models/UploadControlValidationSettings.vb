c25513cd3aea1364bbb10ca4a0f3ac1082f65afa
khlopova.ekaterina <ekaterina.khlopova@devexpress.com>
2018-12-07 17:15:45 +0300

    VB.NET project updated

---
+++ VB/UploadControlExample/Models/UploadControlValidationSettings.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports DevExpress.Web
+
+Public Class MyUploadControlValidationSettings
+    Public Shared Settings As New UploadControlValidationSettings() With {.AllowedFileExtensions = New String() {
+        ".jpg", ".jpeg", ".png", ".bmp", ".pdf", ".xml", ".txt", ".rtf", ".docx", ".xls", ".xlsx"},
+        .MaxFileSize = 4194304}
+End Class
