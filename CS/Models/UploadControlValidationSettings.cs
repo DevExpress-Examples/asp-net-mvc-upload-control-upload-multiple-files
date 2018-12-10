4f268db67a848fb791f5a7d6dfd21037bbbd440a
khlopova.ekaterina <ekaterina.khlopova@devexpress.com>
2018-12-10 14:38:32 +0300

    CS project is updated

--- CS/Models/UploadControlValidationSettings.cs
+++
@@ -1,8 +0,0 @@
-using DevExpress.Web;
-
-public class MyUploadControlValidationSettings {
-    public static UploadControlValidationSettings Settings = new UploadControlValidationSettings() {
-        AllowedFileExtensions = new string[] { ".jpg", ".jpeg", ".png", ".bmp", ".pdf", ".xml", ".txt", ".rtf", ".docx", ".xls", ".xlsx" },
-        MaxFileSize = 4194304
-    };
-}
